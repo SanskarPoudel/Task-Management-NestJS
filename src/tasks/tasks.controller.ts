68524c6e984381fc42c34e6619f8a9a88972e7a7
SanskarPoudel <72004392+SanskarPoudel@users.noreply.github.com>
2023-03-28 18:48:50 +0545

    deleteTask , updateTaskStatus -- createTask using dto

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,14 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { Task } from './tasks.model';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  Delete,
+  Patch,
+} from '@nestjs/common';
+import { CreateTaskDto } from 'src/dto/create-tasks.dto';
+import { Task, TaskStatus } from './tasks.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -11,16 +20,42 @@ export class TasksController {
     return this.tasksService.getAllTasks();
   }
 
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
   //   @Post()
   //   createTask(@Body() body: { title: string; description: string }) {
   //     return this.tasksService.createTask(body.title, body.description);
   //   }
+
   //Alternatively
+
+  // @Post()
+  // createTask(
+  //   @Body('title') title: string,
+  //   @Body('description') description: string,
+  // ): Task {
+  //   return this.tasksService.createTask(title, description);
+  // }
+
+  //Using DTO
   @Post()
-  createTask(
-    @Body('title') title: string,
-    @Body('description') description: string,
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTaskbyId(@Param('id') id: string): Task[] {
+    return this.tasksService.deleteTaskbyId(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
   ) {
-    return this.tasksService.createTask(title, description);
+    return this.tasksService.updateTaskStatus(id, status);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuidv4 } from 'uuid';
+import { CreateTaskDto } from 'src/dto/create-tasks.dto';
+import { Console } from 'console';
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [
@@ -11,7 +13,15 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  getTaskById(id: string): Task {
+    const task = this.tasks.find((task) => {
+      return task.id === id;
+    });
+    return task;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
     const task: Task = {
       id: uuidv4(),
       title,
@@ -19,6 +29,20 @@ export class TasksService {
       status: TaskStatus.OPEN,
     };
     this.tasks.push(task);
+    return task;
+  }
+
+  deleteTaskbyId(id: string): Task[] {
+    const tasks: Task[] = this.tasks.filter((task) => {
+      return task.id !== id;
+    });
+    console.log(tasks);
+    return tasks;
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus) {
+    const task = this.getTaskById(id);
+    task.status = status;
     return task;
   }
 }
